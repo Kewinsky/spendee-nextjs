3f694248e4e62e56ec7bf6c3c68e5de9e41e05dd
Kewinsky <kewin.taoang@gmail.com>
2025-04-12 14:31:34 +0200

    install shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button>Click Me!</Button>
+    </div>
+  );
 };
 
 export default Home;
